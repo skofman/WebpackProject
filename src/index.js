e7b9b06579efcd6c07274b7090dc7fe88cfc4096
Shlomo Kofman <shlomo.kofman@gmail.com>
2017-08-20 23:14:06 -0700

    Webpack dev server

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Routes from './router';
 import '../style/materialize.css';
 import '../style/react-range.css';
 import '../style/style.css';
-console.log('hi!');
+console.log('hello!');
 
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
